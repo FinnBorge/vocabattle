ef01938b7f7eff001d536af7a4d40361a7824f03
Finn Borge <finnborge@gmail.com>
2015-11-23 16:00:43 -0500

    initializing

---
+++ server.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose'),
+    server = require('./lib/createserver')(),
+    MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
+    DBNAME = "vocabattle",
+    PORT = process.env.PORT || 3000;
+
+  /* set controllers */
+  var articleRouter = require('./controllers/article.js');
+  server.use('/article', articleRouter);
+
+  var userRouter = require('./controllers/user.js');
+  server.use('/user', userRouter);
+
+  server.get('/', function(req, res, next){
+  res.redirect(302, '/article/index');
+  });
+
+mongoose.connect(MONGOURI + "/" + DBNAME);
+server.listen(PORT, function(){
+  console.log("SERVER IS UP ON PORT:" + PORT);
+});
